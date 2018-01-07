f027d8ed2ff415ab2dc3b0916cc947ee5761e369
zzy <925383908@qq.com>
2018-01-07 21:54:37 +0800

    读取并输出到文件

--- FakeData/src/fakeData/FakeData.java
+++ FakeData/src/fakeData/FakeData.java
@@ -1,6 +1,7 @@
 package fakeData;
 
 import java.io.IOException;
+import java.util.Random;
 
 import file.ReadFile;
 import file.WriteToFile;
@@ -16,13 +17,30 @@ public class FakeData {
 		ReadFile rfile=new ReadFile ();
 		String pro = null;
 		try {
-			pro=rfile.readFromFileByByte(new String("profession.txt"));
+			pro=rfile.readFromFileByByte(new String("test.txt"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		String[] out=pro.split(new String("¡°£¬¡±"));
+		String[] out=pro.split(new String("\n"));
+		int max=out.length-1;
+	    int min=0;
+	    WriteToFile wfile=new WriteToFile();
+	    Random random = new Random();
+	    for(int i=0;i<14000;i++) {
+	    	int s = random.nextInt(max)%(max-min+1) + min;
+			//System.out.print(s);
+			try {
+				wfile.AddToFileByByte("test2.txt",out[s]+"\r\n");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+		/*
 		WriteToFile wfile=new WriteToFile();
 		try {
 			wfile.WriteToFileByByte("test.txt",out[0]+"\r\n");
@@ -35,6 +53,7 @@ public class FakeData {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		*/
 
 	}
 
